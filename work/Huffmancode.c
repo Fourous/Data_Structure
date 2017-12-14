ccbe935a0776be21b64f16a905c3a4d40c934b87
2120161650 <2120161650>
2017-12-14 18:51:46 +0800

    12.14

--- work/Huffmancode.c
+++ work/Huffmancode.c
@@ -4,11 +4,12 @@
 struct BTreeNode {
   int data;//数据的权值
   char ch;//字符
+  int  chnum[10];
   struct BTreeNode *lchild;
   struct BTreeNode *rchild;
 };
 
-struct BTreeNode* CreateHuffman(int a[], int n,char chk[])
+struct BTreeNode* CreateHuffman(int a[], int n,char charb[])
 {
    int i, j;
    struct BTreeNode **b, *q;
@@ -17,7 +18,7 @@ struct BTreeNode* CreateHuffman(int a[], int n,char chk[])
    {
        b[i] = malloc(sizeof(struct BTreeNode));
        b[i]->data = a[i];
-       b[i]->ch=chk[i];
+       b[i]->ch=charb[i];
        b[i]->lchild = b[i]->rchild = NULL;
    }
    for (i = 1; i < n; i++)//进行 n-1 次循环建立哈夫曼树
@@ -62,7 +63,6 @@ struct BTreeNode* CreateHuffman(int a[], int n,char chk[])
    free(b);
    return q; //返回整个哈夫曼树的树根指针
 }
-
 void HuffManCoding(struct BTreeNode* hfm, int len)//len初始值为0
 {
    static int a[10];
@@ -72,9 +72,11 @@ void HuffManCoding(struct BTreeNode* hfm, int len)//len初始值为0
        if (hfm->lchild == NULL && hfm->rchild == NULL)
        {
            int i;
-           printf("%s的编码：",&hfm->ch);
-           for (i = 0; i < len; i++)
+           printf("结点权值为%d的编码：",hfm->data);
+           for (i = 0; i < len; i++){
+             hfm->chnum[i]=a[i];
                printf("%d", a[i]);
+             }
                printf("\n");
        }
        else
@@ -87,8 +89,37 @@ void HuffManCoding(struct BTreeNode* hfm, int len)//len初始值为0
    }
 }
 
-void HuffManEncoding(int *ch,int n ){
-printf();
+void HuffManEncoding(struct BTreeNode* hfm,int n){
+int select;
+char *cha;
+printf("----------------输入编码字母----------------");
+printf("输入个数        :\n");
+scanf("%d",&select);
+cha=(char *)malloc(select*sizeof(char));
+for (int i = 0; i <select; i++) {
+  printf("输入第%d个字母\n",i );
+  scanf("%s",&cha[i] );
+}
+printf("你输入的字母为:  ");
+for (int j = 0; j < select; j++) {
+  printf("%s",&cha[j] );
+}
+printf("\n");
+printf("编码为:  \n");
+for(int i=0;i<select;i++){
+while(1){
+  if(hfm->ch==cha[i]){
+    for(int j=0;j<10;j++){
+      printf("%d",hfm->chnum[i] );
+      break;
+    }
+    }
+else if(hfm->lchild!=NULL||hfm->rchild!=NULL)
+  hfm=hfm->lchild;
+else if(hfm->lchild!=NULL||hfm->rchild!=NULL)
+  hfm=hfm->rchild;
+}
+}
 }
 void HuffmanDecoding(){
 printf("2\n");
@@ -97,7 +128,7 @@ printf("2\n");
 void printfT(){
   printf("--------------------请输入选项---------------\n");
   printf("1:输入字符,进行编码\n");
-    printf("2:输%s入数字,进行译码\n");
+    printf("2:输入数字,进行译码\n");
       printf("3:退出\n");
 }
 
@@ -107,11 +138,9 @@ void main(void){
   char *b;//字符存储数组
   int n;//节点数
   int select;//选项
-  int chn;//译码长度
-  char *chl;//译码字符串存储数组
   printf("-------------------构造哈弗曼树的字符数--------------------\n");
   scanf("%d",&n );
-  a=(int *)malloc(n*sizeof(int));
+  a=(int *)malloc(n*sizeof(int));//字母的权值
   b=(char *)malloc(sizeof(char));
   for(int i=0;i<n;i++){
     printf("请输入字符:    \n");
@@ -121,7 +150,7 @@ void main(void){
   }
   hfm=CreateHuffman(a,n,b);
   printf("哈弗曼的编码为:    \n");
-  HuffManCoding(hfm,0);
+HuffManCoding(hfm,0);
   while(1){
     printfT();
     scanf("%d",&select );
@@ -129,7 +158,7 @@ void main(void){
       break;
     }
     else if(select==1){
-      HuffManEncoding();
+      HuffManEncoding(hfm,n);
       printf("\n");
       continue;
     }
